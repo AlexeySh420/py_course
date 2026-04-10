79b6206fe9882c1d95b06318033760ce74df0f50
Alexey <shevtsovalexey420@gmail.com>
2026-04-10 14:23:06 +0300

    Lecture 1

---
+++ Alexey_Shevtsov_class_work_L1.py
@@ -0,0 +1,18 @@
+Python 3.13.1 (main, Jan 16 2025, 13:50:41) [GCC 11.4.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 5+5
+10
+>>> hello
+Traceback (most recent call last):
+  File "<python-input-1>", line 1, in <module>
+    hello
+NameError: name 'hello' is not defined. Did you mean: 'help'?
+>>> print("Hello World!")
+Hello World!
+>>> print('Моя первая дз)')
+Моя первая дз)
+>>> print(1,2,3)
+1 2 3
+>>> print('\\\\')
+\\
+>>> 
---
+++ Alexey_Shevtsov_home_work_L1.py
@@ -0,0 +1,20 @@
+# HW 1.0.
+print("Hello, World!")
+
+# HW 1.1.
+print(
+    "    * ",
+    "   * *",
+    "  *****",
+    "   * * ",
+    "  *   *",
+    " *******",
+    "  *   *",
+    "   ***",
+    sep="\n",
+    end="",
+)
+print("\n")
+
+# HW 1.3.
+print('"I\'m"', '""learning""', '"""Python"""', sep="\n")
