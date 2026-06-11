c0f1cfeda449e6339ebbce4e8e1568b311eb6112
AlexeySh420 <shevtsovalexey420@gmail.com>
2026-06-11 12:07:05 +0300

    Lecture 16 CW HW

--- sql_db_example/names.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user (u_name, u_surname) VALUES ("USER", "ONE");
-INSERT INTO user (u_name, u_surname) VALUES ("USER", "TWO");
-INSERT INTO user (u_name, u_surname) VALUES ("USER", "THREE");
-INSERT INTO user (u_name, u_surname) VALUES ("USER", "FOUR");
--- sql_db_example/tasks.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO task (t_name, t_priority, u_id_fk) VALUES ("do homework", 2, 1);
-INSERT INTO task (t_name, t_priority, u_id_fk) VALUES ("cook", 20, 2);
-INSERT INTO task (t_name, t_priority, u_id_fk) VALUES ("go for a walk", 100, 3);
-INSERT INTO task (t_name, t_priority, u_id_fk) VALUES ("buy groceries", 4, 4);
-INSERT INTO task (t_name, t_priority, u_id_fk) VALUES ("sleep", 1, 1);
